9f2cd41f82a64123d1a313a9e20a4c8e3c05470a
gabrielsaimo68 <gabrielsaimo68@gmail.com>
2023-01-18 17:42:40 -0300

    alteração de layout

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,18 +6,26 @@ import CollapseMenu from "./modules/Collapse";
 import Menu from "./modules/BottonMenu";
 function App() {
   return (
-    <div className="App background_fundo">
-      <img
-        className="logo"
-        src={require("../assets/logo.png")}
-        alt="logo-principal"
-        loading="lazy"
-      />
-      <Affix offsetTop={10} style={{ marginLeft: "80%" }}>
-        <Menu />
-      </Affix>
-      <CollapseMenu />
-      <div style={{ height: 30 }} />
+    <div>
+      <div className="App background_fundo">
+        <img
+          className="fundo"
+          src={require("../assets/fundo.png")}
+          alt="fundo"
+          loading="lazy"
+        />
+        <img
+          className="logo"
+          src={require("../assets/logo.png")}
+          alt="logo-principal"
+          loading="lazy"
+        />
+        <Affix offsetTop={10} style={{ marginLeft: "80%" }}>
+          <Menu />
+        </Affix>
+        <CollapseMenu />
+        <div style={{ height: 30 }} />
+      </div>
     </div>
   );
 }
