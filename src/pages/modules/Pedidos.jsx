f37ac9e40706d91def8afa7af4d446688aa5ffd6
gabrielsaimo68 <gabrielsaimo68@gmail.com>
2023-01-20 14:22:11 -0300

    alteração de layout

--- src/pages/modules/Pedidos.jsx
+++ src/pages/modules/Pedidos.jsx
@@ -7,13 +7,7 @@ export default function Pedidos(atualizar) {
   const data = new Date();
   const hora = data.getHours();
   const dataFormatada =
-    data.toLocaleDateString("pt-BR", { timeZone: "UTC" }) +
-    " " +
-    hora +
-    ":" +
-    data.getMinutes() +
-    ":" +
-    data.getSeconds();
+    hora + ":" + data.getMinutes() + ":" + data.getSeconds();
   const firebaseConfig = initializeApp({
     apiKey: "AIzaSyDHuslm5iZZGtOk3ChXKXoIGpQQQI4UaUQ",
     authDomain: "encanto-amapaense.firebaseapp.com",
@@ -60,60 +54,70 @@ export default function Pedidos(atualizar) {
 
   return (
     <>
+      <h1>Atualizado as {dataFormatada}</h1>
       {pedidos.map((pedido) => (
-        <Descriptions title={dataFormatada} layout="vertical" bordered>
-          <Descriptions.Item label="N° do Pedido">
-            {pedido.pedido}
-          </Descriptions.Item>
-          <Descriptions.Item label="Mesa">{pedido.mesa}</Descriptions.Item>
-          <Descriptions.Item label="Automatic Renewal">YES</Descriptions.Item>
-          <Descriptions.Item label="Order time">
-            2018-04-24 18:00:00
-          </Descriptions.Item>
-          <Descriptions.Item label="Usage Time" span={2}>
-            2019-04-24 18:00:00
-          </Descriptions.Item>
-          <Descriptions.Item label="Status" span={3}>
-            <Badge
-              status={
-                pedido.status === "Pendente"
-                  ? "processing"
-                  : pedido.status === "Cancelado"
-                  ? "error"
-                  : pedido.status === "Finalizado"
-                  ? "success"
-                  : pedido.status === "Atrazado"
-                  ? "warning"
-                  : "default"
-              }
-              text={pedido.status}
-            />
-          </Descriptions.Item>
-          <Descriptions.Item label="Preço do pedido">
-            {pedido.valor}
-          </Descriptions.Item>
-          <Descriptions.Item label="Desconto">
-            {pedido.desconto}
-          </Descriptions.Item>
-          <Descriptions.Item label="Preço final">
-            {Number(pedido.valor) - Number(pedido.desconto)}
-          </Descriptions.Item>
-          <Descriptions.Item label="Pedido">
-            {listapedidos.map((listapedido) => (
-              <div>
-                {listapedido.n_pedido === pedido.pedido ? (
-                  <div>
-                    <p>
-                      {listapedido.qdt}x {listapedido.pedido}
-                    </p>
-                  </div>
-                ) : (
-                  <div></div>
-                )}
-              </div>
-            ))}
-          </Descriptions.Item>
-        </Descriptions>
+        <div style={{ marginBottom: 10 }}>
+          <Descriptions
+            bordered
+            column={{
+              xxl: 4,
+              xl: 3,
+              lg: 3,
+              md: 3,
+              sm: 2,
+              xs: 1,
+            }}
+          >
+            <Descriptions.Item label="N° Pedido">
+              {pedido.pedido}
+            </Descriptions.Item>
+            <Descriptions.Item label="Mesa">{pedido.mesa}</Descriptions.Item>
+            <Descriptions.Item label="Hora do pedido">
+              12:33:37
+            </Descriptions.Item>
+            <Descriptions.Item label="Status">
+              <Badge
+                status={
+                  pedido.status === "Pendente"
+                    ? "processing"
+                    : pedido.status === "Cancelado"
+                    ? "error"
+                    : pedido.status === "Finalizado"
+                    ? "success"
+                    : pedido.status === "Atrazado"
+                    ? "warning"
+                    : "default"
+                }
+                text={pedido.status}
+              />
+            </Descriptions.Item>
+            <Descriptions.Item label="Valor do Pedido">
+              R$ {pedido.valor},00
+            </Descriptions.Item>
+            <Descriptions.Item label="Desconto">
+              R$ {pedido.desconto},00
+            </Descriptions.Item>
+
+            <Descriptions.Item label="Valor Total">
+              R$ {Number(pedido.valor) - Number(pedido.desconto)},00
+            </Descriptions.Item>
+            <Descriptions.Item label="Pedido" span={3}>
+              {listapedidos.map((listapedido) => (
+                <div>
+                  {listapedido.n_pedido === pedido.pedido ? (
+                    <div>
+                      <p>
+                        {listapedido.qdt}x {listapedido.pedido}
+                      </p>
+                    </div>
+                  ) : (
+                    <div></div>
+                  )}
+                </div>
+              ))}
+            </Descriptions.Item>
+          </Descriptions>
+        </div>
       ))}
     </>
   );
