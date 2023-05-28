9b9e2112562a0896037281b9c0d31828327b7108
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-05-28 02:09:01 -0300

    teste

--- src/pages/modules/Garçom.jsx
+++ src/pages/modules/Garçom.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, Divider, Input, Modal, Tabs } from "antd";
+import { Button, Card, Divider, Input, Modal, Select } from "antd";
 import "firebase/database";
 import Dashboard from "../modules/Dasboard";
 import Menssagem from "../modules/Menssagem";
 import Pedidos from "../modules/Pedidos";
 import { getUser } from "../../services/user.ws";
+import { getCardapio } from "../../services/cardapio.ws";
+import { getPedidos } from "../../services/Pedidos.ws";
 
 export default function Garçom() {
   const [actionCardapio, setActionCardapio] = useState(true);
@@ -12,18 +14,43 @@ export default function Garçom() {
   const [actionPeido, setActionPedido] = useState(true);
   const [dateUser, setDateUser] = useState();
   const [visible, setVisible] = React.useState(true);
+  const [showModall, setShowModall] = React.useState(false);
   const [acessable, setAcessable] = React.useState(false);
   const [userNome, setUserNome] = useState("");
   const [UserCategoria, setUserCategoria] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [pedidos, setPedido] = useState([]);
+  const [cardapio, setCardapio] = useState([]);
+  const [valor, setValor] = useState(0);
 
   useEffect(() => {
     getCachedDateUser();
+    getPedido();
+    getCardapios();
   }, []);
 
   const acessar = () => {
     GetUsuario();
+  };
+  const getPedido = async () => {
+    const pedidos = await getPedidos();
+    setPedido(pedidos);
+  };
+  const getCardapios = async () => {
+    const cardapio = await getCardapio();
+    setCardapio(cardapio);
+  };
+
+  const handleChange = (selectedValues) => {
+    let totalValor = 0;
+
+    const selectedItem = cardapio.find((item) => item.id === selectedValues);
+    if (selectedItem) {
+      totalValor += parseFloat(selectedItem.price);
+    }
+
+    setValor(totalValor);
   };
 
   const GetUsuario = async () => {
@@ -106,6 +133,10 @@ export default function Garçom() {
     setDateUser(null);
   };
 
+  const showModal = () => {
+    setShowModall(true);
+  };
+
   return (
     <>
       {!acessable ? (
@@ -143,7 +174,49 @@ export default function Garçom() {
               <Button onClick={() => logout()}>Sair</Button>
             </div>
           </div>
-          
+          <div>
+            <Button onClick={() => showModal()}>Adicionar Messa</Button>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {pedidos.map((item) => (
+              <Card
+                title={"Messa " + item.mesa}
+                style={{ width: "100%", marginTop: 16 }}
+                key={item.id}
+                onClick={() => console.log(item)}
+              >
+                <p>{item.status}</p>
+                <p>Valor: {item.valor}</p>
+              </Card>
+            ))}
+          </div>
+          <Modal title="Messa" open={showModall} footer={null}>
+            <div>
+              <label>Messa</label>
+              <Input type="number" />
+
+              <label>Pedido</label>
+              <div>
+                <Select
+                  allowClear
+                  style={{
+                    width: "80%",
+                  }}
+                  placeholder="Selecione aqui"
+                  onChange={handleChange}
+                  options={cardapio.map((item) => ({
+                    value: item.id,
+                    label: item.id + " - " + item.name,
+                  }))}
+                />
+                <Input style={{ width: "20%" }} type="number" placeholder="0" />
+              </div>
+
+              <label>Valor</label>
+              <Input type="number" prefix="R$" disabled value={valor} />
+              <Button>Enviar</Button>
+            </div>
+          </Modal>
         </div>
       )}
     </>
