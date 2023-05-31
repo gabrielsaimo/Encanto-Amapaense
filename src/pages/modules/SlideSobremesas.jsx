cbd51af683db78abaca7441cbb1251a884a14bbf
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-05-31 20:14:37 -0300

    up

--- src/pages/modules/SlideSobremesas.jsx
+++ src/pages/modules/SlideSobremesas.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { Carousel, Image } from "antd";
 import "../../css/Slide.css";
--- src/pages/modules/footer.jsx
+++ src/pages/modules/footer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import logo from "../../assets/saimo.png";
 const Footer = () => {
