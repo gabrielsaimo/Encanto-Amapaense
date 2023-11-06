d01ebfa071f514225d5927a9bf7302698dacc60e
gabrielsaimo <gabrielsaimo68@gmail.com>
2023-11-06 18:07:56 -0300

    Updated Collapse component imports and added lazy loading for images

--- src/pages/modules/Collapse.jsx
+++ src/pages/modules/Collapse.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useMemo, lazy, Suspense } from "react";
-import { Collapse, Image, Carousel } from "antd";
+import { Collapse, Carousel, Spin } from "antd";
 import { CaretRightOutlined } from "@ant-design/icons";
 import LazyLoad from "react-lazyload";
 import "../../css/Collapse.css";
@@ -11,7 +11,9 @@ import { getCardapio, getImgCardapio } from "../../services/cardapio.ws";
 import { getCategoty } from "../../services/category.ws";
 
 const { Panel } = Collapse;
-const LazyLoadedImage = lazy(() => import("antd").then(module => ({ default: module.Image })));
+const LazyLoadedImage = lazy(() =>
+  import("antd").then((module) => ({ default: module.Image }))
+);
 
 const CollapseMenu = () => {
   const [cardapio, setCardapio] = useState([]);
@@ -20,7 +22,7 @@ const CollapseMenu = () => {
 
   useEffect(() => {
     if (cardapio.length === 0) {
-      gtCardapio();
+      getCardapios();
     }
     if (cardapioCategory.length === 0) {
       getCardapioCategory();
@@ -30,7 +32,7 @@ const CollapseMenu = () => {
     }
   }, [cardapio]);
 
-  const gtCardapio = async () => {
+  const getCardapios = async () => {
     const cardapioCollection = await getCardapio();
     setCardapio(cardapioCollection);
   };
@@ -72,7 +74,7 @@ const CollapseMenu = () => {
             {img
               .filter((img1) => img1.idreq && img1.idreq === id)
               .map((img1, index) => (
-                <Suspense key={index} fallback={<div>Carregando...</div>}>
+                <Suspense key={index} fallback={<Spin />}>
                   <LazyLoadedImage
                     src={atob(img1.imagem)}
                     key={index}
@@ -97,11 +99,23 @@ const CollapseMenu = () => {
   const renderSlides = useMemo(() => {
     return (index) => {
       if (index === 0) {
-        return <Suspense fallback={<div>Carregando...</div>}><SlidesPrincipal /></Suspense>;
+        return (
+          <Suspense fallback={<Spin />}>
+            <SlidesPrincipal />
+          </Suspense>
+        );
       } else if (index === 11) {
-        return <Suspense fallback={<div>Carregando...</div>}><SlidesSobemesas /></Suspense>;
+        return (
+          <Suspense fallback={<Spin />}>
+            <SlidesSobemesas />
+          </Suspense>
+        );
       } else if (index === 14) {
-        return <Suspense fallback={<div>Carregando...</div>}><SlidesBebidas /></Suspense>;
+        return (
+          <Suspense fallback={<Spin />}>
+            <SlidesBebidas />
+          </Suspense>
+        );
       }
       return null;
     };
@@ -113,83 +127,84 @@ const CollapseMenu = () => {
       return (
         <div key={key}>
           {renderSlides(index)}
-
-          <Collapse
-            bordered={false}
-            header={item1.name}
-            easing="ease-in-out"
-            waitForAnimate={true}
-            defaultActiveKey={Array.from({ length: 1 }, (_, i) => String(i))}
-            destroyInactivePanel={false}
-            expandIconPosition="end"
-            expandIcon={({ isActive }) => (
-              <CaretRightOutlined rotate={isActive ? 90 : 0} />
-            )}
-            style={{
-              background: "transparent",
-            }}
-          >
-            <Panel
-              id={key}
-              style={{
-                color: "#7a4827",
-                fontWeight: "bold",
-                backgroundImage: `url(${require("../../assets/tinta.webp")}) `,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: 150,
-                backgroundPositionX: "50%",
-                backgroundPositionY: -8,
-              }}
+          <Suspense fallback={<Spin />}>
+            <Collapse
+              bordered={false}
               header={item1.name}
+              easing="ease-in-out"
+              waitForAnimate={true}
+              defaultActiveKey={Array.from({ length: 1 }, (_, i) => String(i))}
+              destroyInactivePanel={false}
+              expandIconPosition="end"
+              expandIcon={({ isActive }) => (
+                <CaretRightOutlined rotate={isActive ? 90 : 0} />
+              )}
+              style={{
+                background: "transparent",
+              }}
             >
-              {cardapio
-                .filter(
-                  (categoria) =>
-                    categoria.category === item1.name && categoria.active
-                )
-                .map((categoria, idx) => (
-                  <div key={idx} className="border">
-                    <div style={{ display: "flex" }}>
-                      {categoria.ids &&
-                        imgSrc.map((img1, index) =>
-                          renderImageCarousel(img1, index, categoria.id)
-                        )}
+              <Panel
+                id={key}
+                style={{
+                  color: "#7a4827",
+                  fontWeight: "bold",
+                  backgroundImage: `url(${require("../../assets/tinta.webp")}) `,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: 150,
+                  backgroundPositionX: "50%",
+                  backgroundPositionY: -8,
+                }}
+                header={item1.name}
+              >
+                {cardapio
+                  .filter(
+                    (categoria) =>
+                      categoria.category === item1.name && categoria.active
+                  )
+                  .map((categoria, idx) => (
+                    <div key={idx} className="border">
+                      <div style={{ display: "flex" }}>
+                        {categoria.ids &&
+                          imgSrc.map((img1, index) =>
+                            renderImageCarousel(img1, index, categoria.id)
+                          )}
 
-                      <div className="flex">
-                        <div style={{ width: "100%", display: "contents" }}>
-                          <div>
-                            <p className="p_1 name georgia-font">
-                              {categoria.name}
-                            </p>
-                          </div>
-                          <div className="flex">
-                            <div className="sub">
-                              {categoria.sub} {categoria.description}
+                        <div className="flex">
+                          <div style={{ width: "100%", display: "contents" }}>
+                            <div>
+                              <p className="p_1 name georgia-font">
+                                {categoria.name}
+                              </p>
+                            </div>
+                            <div className="flex">
+                              <div className="sub">
+                                {categoria.sub} {categoria.description}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                        <div
-                          style={{
-                            display: "flex",
-                            justifyContent: "end",
-                            minWidth: "100%",
-                            alignItems: "flex-end",
-                          }}
-                        >
-                          <p className="p_1 price georgia-bold-font">
-                            {`R$ ${
-                              categoria.price % 1 !== 0
-                                ? categoria.price.replace(".", ",")
-                                : categoria.price + ",00"
-                            }`}
-                          </p>
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "end",
+                              minWidth: "100%",
+                              alignItems: "flex-end",
+                            }}
+                          >
+                            <p className="p_1 price georgia-bold-font">
+                              {`R$ ${
+                                categoria.price % 1 !== 0
+                                  ? categoria.price.replace(".", ",")
+                                  : categoria.price + ",00"
+                              }`}
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                ))}
-            </Panel>
-          </Collapse>
+                  ))}
+              </Panel>
+            </Collapse>
+          </Suspense>
         </div>
       );
     });
