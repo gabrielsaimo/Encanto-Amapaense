5bdddd2bccef6eaabe106a72200910d214feffa5
gabrielsaimo <gabrielsaimo68@gmail.com>
2024-01-20 00:39:37 -0300

    Add react-intersection-observer and implement slide-in animation for CardapioItem

--- src/pages/Components/CardapioItem.jsx
+++ src/pages/Components/CardapioItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Collapse } from "antd";
 import { CaretRightOutlined } from "@ant-design/icons";
+import { useInView } from "react-intersection-observer";
 
 const { Panel } = Collapse;
 
@@ -10,69 +11,91 @@ const CardapioItem = ({
   item,
   memoizedImgSrc,
   renderImageCarousel,
-}) => (
-  <Collapse
-    bordered={false}
-    header={categoryName}
-    easing="ease-in-out"
-    waitForAnimate={true}
-    defaultActiveKey={Array.from({ length: 1 }, (_, i) => String(i))}
-    destroyInactivePanel={false}
-    expandIconPosition="end"
-    expandIcon={({ isActive }) => (
-      <CaretRightOutlined rotate={isActive ? 90 : 0} />
-    )}
-    style={{
-      background: "transparent",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    }}
-  >
-    <Panel id={categoryName} style={categoryStyle} header={categoryName}>
-      {item.map((categoria, idx) =>
-        categoria.type.includes("Cardapio") ? (
-          <div key={idx} className="border">
-            <div style={{ display: "flex" }}>
-              {categoria.ids &&
-                memoizedImgSrc.map((img1, index) =>
-                  renderImageCarousel(img1, index, categoria.id)
-                )}
+}) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Só dispara uma vez quando o item entra na tela
+    rootMargin: "0px 0px -100px 0px", // Margem de visibilidade
+  });
 
-              <div className="flex">
-                <div style={{ width: "100%", display: "contents" }}>
-                  <div>
-                    <p className="p_1 name georgia-font">{categoria.name}</p>
-                  </div>
+  const isEven = item.length % 2 === 0; // Verifica se a quantidade de itens é par
+
+  return (
+    <div ref={ref} className={`cardapio-item ${inView ? "slide-in" : ""}`}>
+      <Collapse
+        bordered={false}
+        header={categoryName}
+        easing="ease-in-out"
+        waitForAnimate={true}
+        defaultActiveKey={Array.from({ length: 1 }, (_, i) => String(i))}
+        destroyInactivePanel={false}
+        expandIconPosition="end"
+        expandIcon={({ isActive }) => (
+          <CaretRightOutlined rotate={isActive ? 90 : 0} />
+        )}
+        style={{
+          background: "transparent",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Panel id={categoryName} style={categoryStyle} header={categoryName}>
+          {item.map((categoria, idx) => {
+            const isEven = item.length % 2 === 0;
+            const isLeftToRight = isEven ? idx % 2 === 0 : idx % 2 !== 0;
+            const animationDirection = isLeftToRight
+              ? "slide-in-left"
+              : "slide-in-right";
+
+            return categoria.type.includes("Cardapio") ? (
+              <div
+                key={idx}
+                className={`border ${inView ? animationDirection : ""}`}
+              >
+                <div style={{ display: "flex" }}>
+                  {categoria.ids &&
+                    memoizedImgSrc.map((img1, index) =>
+                      renderImageCarousel(img1, index, categoria.id)
+                    )}
+
                   <div className="flex">
-                    <div className="sub">
-                      {categoria.sub} {categoria.description}
+                    <div style={{ width: "100%", display: "contents" }}>
+                      <div>
+                        <p className="p_1 name georgia-font">
+                          {categoria.name}
+                        </p>
+                      </div>
+                      <div className="flex">
+                        <div className="sub">
+                          {categoria.sub} {categoria.description}
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "end",
+                        minWidth: "100%",
+                        alignItems: "flex-end",
+                      }}
+                    >
+                      <p className="p_1 price georgia-bold-font">
+                        {`R$ ${
+                          categoria.price % 1 !== 0
+                            ? categoria.price.replace(".", ",")
+                            : categoria.price + ",00"
+                        }`}
+                      </p>
                     </div>
                   </div>
                 </div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "end",
-                    minWidth: "100%",
-                    alignItems: "flex-end",
-                  }}
-                >
-                  <p className="p_1 price georgia-bold-font">
-                    {`R$ ${
-                      categoria.price % 1 !== 0
-                        ? categoria.price.replace(".", ",")
-                        : categoria.price + ",00"
-                    }`}
-                  </p>
-                </div>
               </div>
-            </div>
-          </div>
-        ) : null
-      )}
-    </Panel>
-  </Collapse>
-);
+            ) : null;
+          })}
+        </Panel>
+      </Collapse>
+    </div>
+  );
+};
 
 export default CardapioItem;
