c54974aae8d87aa748fa4902e43e4eeed3d366b3
gabrielsaimo <gabrielsaimo68@gmail.com>
2024-10-02 14:54:43 -0300

    Atualiza a renderização do React e adiciona suporte para relatórios de desempenho

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 /* eslint-disable react/jsx-no-undef */
-import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  (window.location.href =
+    "https://menu-digital.vercel.app/home/2/Encanto%20Amapaense")
+);
+
+reportWebVitals(sendToVercelAnalytics);
+
+/*import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "../src/Routes/Rotas";
 import ReactDOM from "react-dom/client";
@@ -17,4 +29,4 @@ root.render(
   </BrowserRouter>
 );
 
-reportWebVitals(sendToVercelAnalytics);
+reportWebVitals(sendToVercelAnalytics);*/
